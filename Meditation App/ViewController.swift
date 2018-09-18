5bbe87cae5ceadcc4a1d9c74eb9a09effee114d1
bshakir <burhanshakir96@gmail.com>
2018-09-19 01:06:45 +0530

    Initial Commit

---
+++ Meditation App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meditation App
+//
+//  Created by Burhanuddin Shakir on 19/09/18.
+//  Copyright © 2018 meditation-app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
