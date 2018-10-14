27221032bad91f547b1e71cab833b352cbcef4af
bshakir <burhanshakir96@gmail.com>
2018-10-14 15:07:13 +0530

    Settings screen (Scroll View not working)

---
+++ Meditation App/Controllers/SettingsModule/SettingsVC.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsVC.swift
+//  Meditation App
+//
+//  Created by Burhanuddin Shakir on 13/10/18.
+//  Copyright © 2018 meditation-app. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
