d066011f324602f6909346b25bf2ee7fdc99d56c
bshakir <burhanshakir96@gmail.com>
2018-10-31 23:28:56 +0300

    Settings VC

---
+++ Meditation App/Constants/UserDefaultKeyNames.swift
@@ -0,0 +1,20 @@
+//
+//  UserDefaultKeyNames.swift
+//  Meditation App
+//
+//  Created by Burhanuddin Shakir on 31/10/18.
+//  Copyright © 2018 meditation-app. All rights reserved.
+//
+
+import Foundation
+
+struct UserDefaultKeyNames
+{
+    
+    struct Settings
+    {
+        static let chakraCuningSetting = "Chakra Cuning Settings"
+        static let sourceCodeSetting = "Source Code Settings"
+        static let gspaceSetting = "G Space Settings"
+    }
+}
--- Meditation App/Controllers/SettingsModule/SettingsVC.swift
+++ Meditation App/Controllers/SettingsModule/SettingsVC.swift
@@ -8,24 +8,110 @@
 
 import UIKit
 
-class SettingsVC: UIViewController {
+class SettingsVC: UIViewController
+{
 
     @IBOutlet weak var scrollView: UIScrollView!
-    
     @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var chakraCuningTab: UIControl!
+    @IBOutlet weak var sourceCodeTab: UIControl!
+    @IBOutlet weak var gspaceTab: UIControl!
+    
+    @IBOutlet weak var setTimerLbl: UILabel!
+    @IBOutlet weak var timerLbl: UILabel!
+    @IBOutlet weak var optimizeLblTopConstraint: NSLayoutConstraint!
+    
+    // By default chakra cuning settings selected
+    var settingForMeditation : String = UserDefaultKeyNames.Settings.chakraCuningSetting
+    
+    // Setting up scroll view
+    override func viewDidLayoutSubviews()
+    {
+        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: 750)
+    }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
     }
     
-    override func viewDidLayoutSubviews()
+    // MARK :- IBAction Functions
+    @IBAction func chakraCuningPressed(_ sender: Any)
     {
-        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: 851)
-//        bgImg.frame = CGRect(x: 0, y: 0, width: scrollView.contentSize.width, height: 10)
+        handleColorChange(forTab: "chakraCuning")
+        doHideTimer(boolean: false)
     }
-
-
+    
+    @IBAction func sourceCodePressed(_ sender: Any)
+    {
+        handleColorChange(forTab: "sourceCode")
+        doHideTimer(boolean: true)
+    }
+    
+    @IBAction func gspacePressed(_ sender: Any)
+    {
+        handleColorChange(forTab: "gSpace")
+        doHideTimer(boolean: true)
+    }
+    
+    @IBAction func settingsSavePressed(_ sender: Any)
+    {
+        
+    }
+    
+    // MARK :- UI Changes for tab changes
+    func handleColorChange(forTab tabName : String)
+    {
+        switch tabName
+        {
+            case "chakraCuning":
+                print("Chakra Cuning")
+            
+                chakraCuningTab.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.631372549, blue: 1, alpha: 1)
+                sourceCodeTab.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                gspaceTab.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            
+            case "sourceCode":
+                print("Source Code")
+            
+                chakraCuningTab.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                sourceCodeTab.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.631372549, blue: 1, alpha: 1)
+                gspaceTab.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            
+            case "gSpace":
+                print("G Space")
+            
+                chakraCuningTab.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                sourceCodeTab.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                gspaceTab.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.631372549, blue: 1, alpha: 1)
+            
+            default:
+                print("No tabs chosen")
+        }
+    }
+    
+    func doHideTimer(boolean : Bool)
+    {
+        setTimerLbl.isHidden = boolean
+        timerLbl.isHidden = boolean
+        
+        // If timer is hidden change constraints and scroll size
+        if boolean
+        {
+            optimizeLblTopConstraint.constant = 20
+            scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: 620)
+            
+        }
+        else
+        {
+            optimizeLblTopConstraint.constant = 145
+            scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: 750)
+        }
+        
+        scrollView.layoutIfNeeded()
+    }
+    
 }
