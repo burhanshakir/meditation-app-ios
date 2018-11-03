0c5c42c11a5a1c2644f78785864f086c256539e6
bshakir <burhanshakir96@gmail.com>
2018-11-03 18:11:26 +0530

    Set timer screen

--- Meditation App/Controllers/SettingsModule/SettingsVC.swift
+++ Meditation App/Controllers/SettingsModule/SettingsVC.swift
@@ -17,8 +17,11 @@ class SettingsVC: UIViewController
     @IBOutlet weak var sourceCodeTab: UIControl!
     @IBOutlet weak var gspaceTab: UIControl!
     
+    @IBOutlet weak var timerBtn: UIButton!
+    @IBOutlet weak var musicSwitch: UISwitch!
+    @IBOutlet weak var reminderSwitch: UISwitch!
+    @IBOutlet weak var landscapeSwitch: UISwitch!
     @IBOutlet weak var setTimerLbl: UILabel!
-    @IBOutlet weak var timerLbl: UILabel!
     @IBOutlet weak var optimizeLblTopConstraint: NSLayoutConstraint!
     
     // By default chakra cuning settings selected
@@ -41,26 +44,50 @@ class SettingsVC: UIViewController
     // MARK :- IBAction Functions
     @IBAction func chakraCuningPressed(_ sender: Any)
     {
+        settingForMeditation = UserDefaultKeyNames.Settings.chakraCuningSetting
         handleColorChange(forTab: "chakraCuning")
         doHideTimer(boolean: false)
     }
     
     @IBAction func sourceCodePressed(_ sender: Any)
     {
+        settingForMeditation = UserDefaultKeyNames.Settings.sourceCodeSetting
         handleColorChange(forTab: "sourceCode")
         doHideTimer(boolean: true)
     }
     
     @IBAction func gspacePressed(_ sender: Any)
     {
+        settingForMeditation = UserDefaultKeyNames.Settings.gspaceSetting
         handleColorChange(forTab: "gSpace")
         doHideTimer(boolean: true)
     }
     
+    
     @IBAction func settingsSavePressed(_ sender: Any)
     {
+        var settings = [String : Any]()
+        
+        if(settingForMeditation == UserDefaultKeyNames.Settings.chakraCuningSetting)
+        {
+            settings.updateValue(timerBtn.titleLabel?.text! ?? "", forKey: "timer")
+        }
+        
+        settings.updateValue(musicSwitch.isOn, forKey: "music")
+        settings.updateValue(landscapeSwitch.isOn, forKey: "landscape")
+        settings.updateValue(reminderSwitch.isOn, forKey: "reminder")
+        
+        UserDefaults.standard.set(settings, forKey: settingForMeditation)
+        
+        print("\(UserDefaults.standard.value(forKey: settingForMeditation)!)")
         
     }
+    
+    @IBAction func timerPressed(_ sender: Any)
+    {
+        self.performSegue(withIdentifier: "ShowTimerVC", sender: nil)
+    }
+    
     
     // MARK :- UI Changes for tab changes
     func handleColorChange(forTab tabName : String)
@@ -95,9 +122,6 @@ class SettingsVC: UIViewController
     
     func doHideTimer(boolean : Bool)
     {
-        setTimerLbl.isHidden = boolean
-        timerLbl.isHidden = boolean
-        
         // If timer is hidden change constraints and scroll size
         if boolean
         {
@@ -111,7 +135,15 @@ class SettingsVC: UIViewController
             scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: 750)
         }
         
-        scrollView.layoutIfNeeded()
+        // Changing constraints with animations
+        UIView.animate(withDuration: 0.5)
+        {
+            self.scrollView.layoutIfNeeded()
+        }
+        
+        setTimerLbl.isHidden = boolean
+        timerBtn.isHidden = boolean
+        
     }
     
 }
---
+++ Meditation App/Controllers/SettingsModule/ShowTimerVC.swift
@@ -0,0 +1,49 @@
+//
+//  ShowTimerVC.swift
+//  Meditation App
+//
+//  Created by Burhanuddin Shakir on 01/11/18.
+//  Copyright © 2018 meditation-app. All rights reserved.
+//
+
+import UIKit
+
+class ShowTimerVC: UIViewController {
+
+    @IBOutlet weak var timerPicker: UIDatePicker!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        setUpTimer()
+        
+        
+    }
+    
+    // MARK : - UI changes to timer
+    func setUpTimer()
+    {
+        
+        timerPicker.countDownDuration = 60.0
+        // Change time picker text color
+        timerPicker.setValue(UIColor.white, forKey: "textColor")
+    }
+    
+    // MARK : - IBOutlets
+    
+    @IBAction func cancelPressed(_ sender: Any)
+    {
+        self.navigationController?.popViewController(animated: false)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func savePressed(_ sender: Any)
+    {
+        print(timerPicker.countDownDuration)
+    }
+    
+
+}
